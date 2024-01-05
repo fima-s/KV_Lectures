0cdb852fe1c6b239c33e49f1867e357f5e262fac
Fima Shevrin <efim.shevrin@virtuozzo.com>
2024-01-06 01:13:33 +0800

    Заметки по первой лекции по toolchain

---
+++ Toolchain/Lect1/code/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+
+int main() {
+
+    std::ofstream output_file("testing spaces.txt");
+    output_file << " 1" << " 222 222     " << " 7 a 7   7" << "999" << std::endl;
+    output_file << " 4" << " 5687     " << std::endl;
+
+    std::ifstream input_file("testing spaces.txt");
+    std::vector<int> test{std::istream_iterator<int>(input_file), std::istream_iterator<int>()};
+
+    
+
+	std::cout << "hello ! \n";
+	return 0;
+}
